54d424f6b0fd7ddd4be1aef634cbc80c1e60233a
Brandor Medina <brandormedina16@gmail.com>
2018-12-05 18:32:04 -0400

    Navbar

---
+++ Final/Final/Models/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Login
+    {
+        public int id { get; set; }
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string telefono { get; set; }
+        public string usuario { get; set; }
+        public string clave { get; set; }
+
+
+
+        public virtual Doctor Doctor { get; set; }
+        public virtual Asistentes Asistentes { get; set; }
+    }
+}
