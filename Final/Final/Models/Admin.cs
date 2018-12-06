c628dacee4d3df272dc0728e3278b94797195f1a
Brandor Medina <brandormedina16@gmail.com>
2018-12-05 20:49:09 -0400

    Finish

---
+++ Final/Final/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final.Models
+{
+    public class Admin
+    {
+        public string usario { get; set; }
+        public string clave { get; set; }
+    }
+}
